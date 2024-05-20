53a53c8800a3870fb63fe0951ca55e5cf8301cd6
Greg <gajdukow@protonmail.com>
2024-05-20 16:06:36 +0200

    implement server routing

--- server/app.js
+++ server/app.js
@@ -3,6 +3,10 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 
+// import JSON files
+const cohorts = require("./cohorts.json")
+const students = require("./students.json")
+
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
@@ -29,6 +33,13 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
+
+app.get("/api/students", (req, res) => {
+  res.json(students);
+});
 
 // START SERVER
 app.listen(PORT, () => {
