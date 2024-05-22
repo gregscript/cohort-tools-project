a889ebd6505643b13ccb5cf5f9cdd4875e808879
Greg <gajdukow@protonmail.com>
2024-05-22 16:05:06 +0200

    adjusted student model

--- server/models/Student.model.js
+++ server/models/Student.model.js
@@ -14,7 +14,7 @@ const studentSchema = new Schema({
   program: { type: String, enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"] },
   background: {type: String, default: ""},
   image: {type: String, default: "https://i.imgur.com/r8bo8u7.png"},
-  // cohort: String, (this should be ObjectID of the cohort??????? How??)
+  cohort: {type: mongoose.Schema.Types.ObjectId, ref: 'Cohort'}, 
   projects: Array
 });
  
